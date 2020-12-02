83dd866b825bca4546223e26a3b97fdaefd46103
ZYunH <zyunhjob@163.com>
2020-12-02 13:23:06 +0000

    quickfix

--- clean.sh
+++ clean.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-SCOPE=$1
-
 rm -rf go
-if [ "$SCOPE"x = "all"x ]; then
+if [ "$1"x = "all"x ]; then
   rm -rf golang-go
   rm -rf *.tar.gz
 fi
--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$1"x = "local"x ]; then
+    git clone http://192.168.3.30:3000/zhangyunhao/go.git
+    mv go golang-go
+    exit 0
+fi
+
+
 git clone https://github.com/golang/go.git
 mv go golang-go
