eba6e78f92e8cf057377e5cac9491aa5ff97b2a5
ZhangYunHao <zhangyunhao116@gmail.com>
2021-02-01 17:45:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ set -e
 
 INSTALL_PACKAGE=$1
 if [ -z "${INSTALL_PACKAGE}" ]; then INSTALL_PACKAGE=results/go.tar.gz; fi
-tar -C /usr/local -xzf "${INSTALL_PACKAGE}"
+DIR_NAME=`tar -tzf "${INSTALL_PACKAGE}" | head -1 | cut -f1 -d"/"`
+sudo rm -rf /usr/local/"${DIR_NAME}"
+sudo tar -C /usr/local -xzf "${INSTALL_PACKAGE}"
